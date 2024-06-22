187cef0b1baf0219330ef955433a02dfb9226975
Sai Hlyan Phyoe <sai843hp@prestigein.com>
2024-06-22 15:54:07 +0630

    Setup: Install Tailwind

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Test 15
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
