077ac1ba840ecc5189567493311b2cbb60578419
Sai Hlyan Phyoe <sai843hp@prestigein.com>
2024-06-22 16:04:30 +0630

    Setup: Antd design

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    Test 15
+    Test 16
   </div>
+  <a-button @click="handleMessage">
+    button
+  </a-button>
 </template>
 
-<script>
-export default {
-  setup () {
-    
-
-    return {}
-  }
+<script setup lang="ts">
+const handleMessage = () => {
+  message.info("This is a normal message");
 }
 </script>
 
